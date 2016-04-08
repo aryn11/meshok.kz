b06ad99aca4e9afea2e2529301938f5f12f300d1
aryn11 <aryn11@mail.ru>
2016-04-08 17:24:23 +0600

    Кнопка назад в просмотре профиля

--- htdocs/meshok/logged/user.php
+++ htdocs/meshok/logged/user.php
@@ -64,6 +64,7 @@ $rowUser = mysqli_fetch_array($queryUsers);
                     </tr>
                 <?php } ?>
             </table>
+            <p align="center"><a href="#" onclick="window.history.back();" id="arrow_back"><img src="images/ic_arrow.png"></a></p>
 
         </div>
     </div>
